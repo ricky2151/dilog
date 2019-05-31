54a5acae9ad9e7342c99be1c524cebb96be60e88
Kevin Giovanni <kevin.giovanni@ti.ukdw.ac.id>
2019-05-31 20:20:50 +0700

    test pwa 4

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,7 +15,7 @@
     <script type="text/javascript">
         // Initialize the service worker
         if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register(window.location.host + '/serviceworker.js', {
+            navigator.serviceWorker.register('serviceworker.js', {
                 scope: '.'
             }).then(function (registration) {
                 // Registration was successful
@@ -26,7 +26,6 @@
             });
         }
     </script>
-    @laravelPWA
 </head>
 <body>
     <div id="app">
