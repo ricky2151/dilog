879ff19e5057b2247f49c21e607528e488cd0b43
Thomas <thomas.widiarya@ti.ukdw.ac.id>
2019-06-13 21:34:16 +0700

    update api category price selling

--- app/Models/CategoryPriceSelling.php
+++ app/Models/CategoryPriceSelling.php
@@ -10,7 +10,7 @@ class CategoryPriceSelling extends Model
     use SoftDeletes;
     
     protected $fillable = [
-        'name','diskon'
+        'name','discount'
     ];
 
     public function priceSellings(){
