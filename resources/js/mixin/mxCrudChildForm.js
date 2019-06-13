2e0dcb9b28e379520055c1c76263230942e99a29
samuel ricky <komputerindo21@gmail.com>
2019-06-13 22:31:14 +0700

    fix price format

--- resources/js/mixin/mxCrudChildForm.js
+++ resources/js/mixin/mxCrudChildForm.js
@@ -2,21 +2,25 @@ export default {
 	methods:{
         strToPrice(angka,prefix)
         {
+            //100000
+            //9.000
             angka = angka.toString();
-            var number_string = angka.replace(/[^,\d]/g, '').toString(),
-            split           = number_string.split(','),
-            sisa            = split[0].length % 3,
-            rupiah          = split[0].substr(0, sisa),
-            ribuan          = split[0].substr(sisa).match(/\d{3}/gi);
- 
-            // tambahkan titik jika yang di input sudah menjadi angka ribuan
-            if(ribuan){
-                separator = sisa ? '.' : '';
-                rupiah += separator + ribuan.join('.');
+            var hasil = "";
+            var counter = 0;
+            for(var i = angka.length - 1;i>= 0;i--)
+            {
+                counter++;
+                if(counter % 3 == 0)
+                {
+                    if(i != 0)
+                        hasil = "." + angka.charAt(i) +  hasil;
+                }
+                else
+                {
+                    hasil = angka.charAt(i) + hasil;
+                }
             }
- 
-            rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
-            return prefix == undefined ? rupiah : (rupiah ? 'Rp. ' + rupiah : '');
+            return prefix + hasil;
         },
 		findDataById(id, justreturnno = false)
 		{
