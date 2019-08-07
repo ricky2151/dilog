e52f62be458653d081b33accd4c2288e4fd32fac
samuel ricky <komputerindo21@gmail.com>
2019-08-07 12:34:13 +0700

    Add additional information for datatable component

--- resources/js/components/datatable/cpDatatable.vue
+++ resources/js/components/datatable/cpDatatable.vue
@@ -7,45 +7,54 @@
             :search="prop_search_data"
             class="datatable"
         >
-	        <template v-slot:items="props">
-	            <td>{{ props.item.no }}</td>
-	            <td v-for='(obj,index) in prop_infoDatatable'>
-	            {{ obj.column?props.item[obj.column]:calculate_custom_value(props.item,obj.value) }}
-	        	</td>
-	            
-	            <td>
-	                <div class="text-xs-left">
-	                    <v-menu offset-y>
-	                      <template v-slot:activator="{ on }">
-	                        <v-btn
-	                          color="primary"
-	                          dark
-	                          v-on="on"
-	                          class='btnaction'
-	                        >
-	                          Action
-	                        </v-btn>
-	                      </template>
-	                      <v-list>
-	                        <v-list-tile
-	                          v-for="(item, index) in prop_action_items"
-	                          :key="index"
-	                          v-on:click="$emit('action_clicked',props.item.id,index)"
-	                        >
-	                          <v-list-tile-title>{{ item }}</v-list-tile-title>
-	                        </v-list-tile>
-	                      </v-list>
-	                    </v-menu>
-	                </div>
-	                
-
-	            </td>
-	        </template>
+        	
+	        	
+			<template v-slot:items="props">
+		    	<template v-if='
+    				(prop_filter_by_user_value && prop_get_unique_value && props.item[prop_get_unique_value] == prop_filter_by_user_value) 
+    				||
+    				(!prop_filter_by_user_value) '
+    			>
+			        <td>{{ props.item.no }}</td>
+			        <td v-for='(obj,index) in prop_infoDatatable'>
+						{{ obj.column?props.item[obj.column]:calculate_custom_value(props.item,obj.value) }}
+			    	</td>
+			        <td>
+			            <div class="text-xs-left">
+			                <v-menu offset-y>
+			                  <template v-slot:activator="{ on }">
+			                    <v-btn
+			                      color="primary"
+			                      dark
+			                      v-on="on"
+			                      class='btnaction'
+			                    >
+			                      Action
+			                    </v-btn>
+			                  </template>
+			                  <v-list>
+			                    <v-list-tile
+			                      v-for="(item, index) in prop_action_items"
+			                      :key="index"
+			                      v-on:click="$emit('action_clicked',props.item.id,index, props.item)"
+			                    >
+			                      <v-list-tile-title>{{ item }}</v-list-tile-title>
+			                    </v-list-tile>
+			                  </v-list>
+			                </v-menu>
+			            </div>
+			        </td>
+			    </template>
+			</template>
         </v-data-table>
 	</div>
 </template>
 <script>
+	import mxVariableProcess from '../../mixin/mxVariableProcess';
 	export default {
+		mixins:[
+		    mxVariableProcess
+		],
 		props:[
 			'prop_header',
 			'prop_search_data',
@@ -53,7 +62,9 @@
 			'prop_action_items',
 			'prop_plural_name',
 			'prop_url_index',
-			'prop_filter'
+			'prop_filter',
+			'prop_get_unique_value',
+			'prop_filter_by_user_value',
 		],
 		data () {
 			return {
@@ -65,6 +76,21 @@
 			}
 		},
 		methods: {
+
+			send_unique_value()
+			{
+				var result = [];
+				for(var i = 0;i<this.data_table.length;i++)
+				{
+					result[i] = this.data_table[i][this.prop_get_unique_value];
+				}
+				console.log('filter_by_user 1');
+				console.log(result);
+				result = result.filter((v, i, a) => a.indexOf(v) === i); 
+				console.log('filter_by_user 2');
+				console.log(result);
+				this.$emit('response_unique_value', result);
+			},
 			get_data() {
 
 	            axios.get(this.prop_url_index, {
@@ -73,7 +99,10 @@
 	                }
 	            },this.header_api).then((r) => {
 	            	this.showTable(r);
-	            	console.log(r.data);
+	            	if(this.prop_get_unique_value)
+	            	{
+	            		this.send_unique_value();
+	            	}
 	            	for(var i = 0;i<this.data_table.length;i++)
 	            	{
 	            		this.data_table[i].no = this.data_table.length - i;
@@ -82,7 +111,39 @@
 	        },
 	        showTable(r)
         	{
-	            this.data_table = r.data.items[this.prop_plural_name];
+        		
+        		var temp_r = r.data.items[this.prop_plural_name];
+        		if(this.prop_filter)
+        		{
+        			Object.keys(this.prop_filter).map(function(key, index) {
+        				var temp_column_filter = key;
+						var temp_value_filter = this.prop_filter[key];
+	        			for(var i = 0;i<temp_r.length;i++)
+			            {
+			                if(temp_r[i][temp_column_filter] == temp_value_filter)
+			                {
+			                    this.data_table.push(temp_r[i]);
+			                }
+			            }
+					});
+        			
+        		}
+        		// else if(this.prop_filter_by_user_value && this.prop_get_unique_value)
+        		// {
+        		// 	console.log('harusnya masuk sini');
+        		// 	for(var i = 0;i<temp_r.length;i++)
+		        //     {
+		        //     	console.log(i);
+		        //         if(temp_r[i][this.prop_get_unique_value] == this.prop_filter_by_user_value)
+		        //         {
+		        //             this.data_table.push(temp_r[i]);
+		        //         }
+		        //     }
+        		// }
+        		else
+        		{
+		            this.data_table = temp_r;
+        		}
 	        },
 
 			calculate_custom_value(data, value)
@@ -105,8 +166,12 @@
 
 			},
 		},
+		watch : {
+
+		},
 		mounted(){
 			this.get_data();
-		}
+		},
+		
 	}
 </script>
--- resources/js/components/datatable/cpHeader.vue
+++ resources/js/components/datatable/cpHeader.vue
@@ -1,16 +1,41 @@
 <template>
 	<div>
-		<v-layout row class='bgwhite margintop10'>
-            <v-flex xs6>
+		<v-layout row class='bgwhite'>
+            <v-flex xs8>
                 <div class='marginleft30 margintop10'>
                     <v-icon class='icontitledatatable'>{{prop_icon}}</v-icon>
                     <h2 class='titledatatable'>{{prop_title}} Data</h2>
-                    <v-btn v-on:click='add_clicked(-1)' color="primary" dark class='btnadddata'>
-                    Add Data
-                </v-btn>
+                    
                 </div>
                 
             </v-flex>
+        </v-layout>
+
+        <!-- additional information/data -->
+        <v-layout row v-if='prop_information && prop_format_information' class='margintop10'>
+        	<template v-for='(format, key) in prop_format_information'>
+	        	<v-flex xs1 class='marginleft30'>
+	        		<b>{{key}} :</b>
+	        	</v-flex>
+	        	<v-flex xs7>
+	        		<b>{{ prop_information[format] }}</b>
+	        	</v-flex>
+        	</template>
+        </v-layout>
+
+        <v-layout row>
+        	<v-flex xs8 class='marginleft30'>
+        		<v-select
+				v-if='prop_filter_by_user_format && prop_filter_by_user_ref'
+				:label='prop_filter_by_user_format.title'
+				@change='filter_by_user_change'
+
+				:items='prop_filter_by_user_ref'
+				
+
+				>
+				</v-select>
+        	</v-flex>
             <v-flex xs12 class="text-xs-right">
                 <v-text-field
                     class='d-inline-block searchdatatable'
@@ -20,13 +45,22 @@
                     single-line
                     hide-details
                 ></v-text-field>
+
             </v-flex>
+        </v-layout>
+        <v-layout justify-end>
+        	
+        	<v-btn v-for='(button_name, index) in prop_button_on_index' :key='index' v-on:click='$emit("button_index_clicked", index)' color="primary" dark >
+                {{button_name}}
+            </v-btn>
+        	
+            
         </v-layout>
     </div>
 </template>
 <script>
 	export default {
-		props : ['prop_icon', 'prop_title', 'prop_search_data'],
+		props : ['prop_icon', 'prop_title', 'prop_search_data', 'prop_button_on_index','prop_information', 'prop_format_information', 'prop_filter_by_user_format', 'prop_filter_by_user_ref'],
 		data () {
 			return {
 				search_data :null,
@@ -40,9 +74,9 @@
 		},
 		methods:
 		{
-			add_clicked()
+			filter_by_user_change(val)
 			{
-				this.$emit('add_clicked');
+				this.$emit('filter_by_user_change', val);
 			}
 		}
 	}
